14355385bb571e0a2c9cff43c06a10e991cadf3d
107124 <kredfox4@gmail.com>
2019-07-09 22:25:56 -0600

    Added portfolio-container.js, modified components/app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from "./portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Kent Taylor Portfolio</h1>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        <PortfolioContainer/>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
   }
